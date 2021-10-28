0e809ec076050de12955c4d1faa06182438fe154
Filip Gieryk <filipgcs@gmail.com>
2021-10-29 00:32:18 +0200

    lab z pierwszych cwiczen

---
+++ lab1.py
@@ -0,0 +1,52 @@
+# 1
+# tekst = 'Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniaczw przemyśle poligraficznym. Został po raz pierwszy użyty w XVw. przez nieznanego drukarza do wypełnienia tekstem próbnejksiążki. Pięć wieków później zaczął być używany przemyśleelektronicznym, pozostając praktycznie niezmienionym.Spopularyzował się w latach 60. XX w. wraz z publikacjąarkuszy Letrasetu, zawierających fragmenty Lorem Ipsum,a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji drukówna komputerach osobistych, jak Aldus PageMaker'
+# 2
+# Filip Gieryk
+# nazwisko = (tekst.count('e'))
+# imie = (tekst.count('i'))
+# print('w tekscie jest ' + str(nazwisko) + ' liter e oraz '+ str(imie) +' liter i')
+# e, i 
+
+# 4
+# zmienna_typu_string='zadanie 4 cwiczneia 1'
+# print(dir(zmienna_typu_string))
+# help(zmienna_typu_string.isspace)
+
+# 5 
+# print('Filip Gieryk'[::-1].title())
+
+# 6
+# lista=[1,2,3,4,5,6,7,8,9,10]
+# lista2=lista[5:10]
+# lista=lista[0:5]
+# print(lista2)
+# print(lista)
+
+# 7
+# lista.extend(lista2)
+# lista.insert(0,0)
+# kopia = lista.copy()
+# kopia.sort(reverse=True)
+# print(kopia)
+
+# 8
+# nrindeksu = (('155373', 'Filip Gieryk'), ('143562', 'Jan Kowalski'))
+
+# 9
+# dct = dict(nrindeksu)
+# print(dct['155373'])
+
+
+# 10
+# numeryTel=[234989053,342123435,654634754,5654645654,234989053]
+# unikalneNumeryTel=set(numeryTel)
+# print(numeryTel)
+# print(unikalneNumeryTel)
+
+# 11
+# for i in range (1,11):
+  # print(i)
+
+# 12
+# for i in range (100,20,-5):
+  # print(i)
